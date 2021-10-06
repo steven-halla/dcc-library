08af1e632f20488c5c718eb6890445d7bcfad96c
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-06 12:28:29 -0700

    "created button to cycle through books"

--- src/components/ App.jsx
+++ src/components/ App.jsx
@@ -15,6 +15,17 @@ export class App extends Component {
     };
   }
 
+  goToNextBook = () => {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber ++;
+    if(tempBookNumber === this.books.length) {
+      tempBookNumber = 0;
+    }
+    this.setState({
+      bookNumber: tempBookNumber
+    });
+  }
+
   render(){
     return (
       <div className="container-fluid">
@@ -33,7 +44,7 @@ export class App extends Component {
             <h4>{this.books[this.state.bookNumber].author}</h4>
           </div>
           <div className="col-md-4">
-
+            <button onClick={this.goToNextBook}>Next Book</button>
           </div>
 
 
